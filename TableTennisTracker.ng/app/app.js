ba825d5148000fdd795b29f6962810a4780ee08e
marcvermeir <marcvermeir@pandora.be>
2017-01-07 23:10:34 +0100

    20170107

--- TableTennisTracker.ng/app/app.js
+++ TableTennisTracker.ng/app/app.js
@@ -1,5 +1,9 @@
 var app = angular.module('myApp', ['ngRoute', 'ui.bootstrap', 'ngAnimate']);
 
+app.constant('VTTLAPI', { 'URL' : 'http://api.vttl.be/0.7/index.php?s=vttl', 
+                          'WSDL' : 'http://api.vttl.be/0.7/?wsdl', 
+                          'NAMESPACEURL' : 'http://api.frenoy.net/TabTAPI' });
+
 app.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider
--- TableTennisTracker.ng/app/criteriaCtrl.js
+++ TableTennisTracker.ng/app/criteriaCtrl.js
@@ -1,139 +1,133 @@
-app.controller('criteriaCtrl', function ($scope, $modal, $filter, $location, DataService) {
-    
-    $scope.criteria = { selectedTeam : '', selectedDivision : '', selectedWeek : '' };
+app.controller('criteriaCtrl', function($scope, $modal, $filter, $location, VTTLAPI) {
 
-    /*
-    var ttteams = [
+    $scope.criteria = { selectedTeam: '', selectedDivision: '', selectedWeek: '' };
+
+    //TODO: define 'season' parameter :
+    var season = '16';
+
+    /* var ttteams = [
       { value: "0",    label: "Alle" },
       { value: "287",  label: "A000 - Individueel Antwerpen" },
       { value: "34",   label: "A003 - KTTC Salamander Mechelen" },
-      { value: "187",  label: "A008 - TTC Ekerse" }    
-    ];
-    */
+      { value: "187",  label: "A008 - TTC Ekerse" }     ]; */
 
-    /*
-    var ttdivisions = [
-      { value: "2378", label: "Super Afdeling - Heren" },
-      { value: "2257", label: "Afdeling 1A - Nationaal - Heren" },
-      { value: "2258", label: "Afdeling 1B - Nationaal - Heren" },
-      { value: "2259", label: "Afdeling 2A - Nationaal - Heren" }
-    ];
-    */
+    /* var ttdivisions = [
+      { value: "2378", label: "Super Afdeling - Heren" }, ]; */
 
-    var ttweeks = [
-      { value: "1", label: "Week 1" },
-      { value: "2", label: "Week 2" },
-      { value: "3", label: "Week 3" },
-      { value: "4", label: "Week 4" },
-      { value: "5", label: "Week 5" },
-      { value: "6", label: "Week 6" },
-      { value: "7", label: "Week 7" },
-      { value: "8", label: "Week 8" },
-      { value: "9", label: "Week 9" },
-      { value: "10", label: "Week 10" },
-      { value: "11", label: "Week 11" },
-      { value: "12", label: "Week 12" },
-      { value: "13", label: "Week 13" },
-      { value: "14", label: "Week 14" },
-      { value: "15", label: "Week 15" },
-      { value: "16", label: "Week 16" }
-    ];
+    $scope.fetchTeams = function(season) {
+        'use strict';
 
-    // $scope.ttteams = ttteams;
-    // $scope.ttdivisions = ttdivisions;
-    // $scope.ttweeks = ttweeks;
+        /*
+        var URL = 'http://api.vttl.be/0.7/index.php?s=vttl';
+        var WSDL = 'http://api.vttl.be/0.7/?wsdl';
+        var NAMESPACEURL = 'http://api.frenoy.net/TabTAPI';
+        */
 
-    // https://danmartensen.svbtle.com/data-model-architecture-in-angular
+        $.soap({
+            url: VTTLAPI.URL,
+            type: 'POST',
+            method: 'GetClubs',
+            namespaceQualifier: '',
+            namespaceURL: VTTLAPI.NAMESPACEURL,
+            noPrefix: true,
+            elementName: 'GetClubs',
+            appendMethodToURL: false,
+            soap12: false,
+            context: document.body,
+            data: {
+                Season: season,
+            },
+            success: function(SOAPResponse) {
+                var result = [];
+                var json = SOAPResponse.toJSON();
+                if (json) {
+                    var count = parseInt(json['#document']['SOAP-ENV:Envelope']['SOAP-ENV:Body']['ns1:GetClubsResponse']['ns1:ClubCount']);
+                    var entries = json['#document']['SOAP-ENV:Envelope']['SOAP-ENV:Body']['ns1:GetClubsResponse']['ns1:ClubEntries'];
 
-    $scope.fetchTeams = function () {
+                    result = entries.map(function(entry) {
+                        var ui = entry['ns1:UniqueIndex'];
+                        var ln = entry['ns1:LongName'];
+                        return { 'value': ui, 'label': ui + ' - ' + ln };
+                    });
+                };
 
-      //TODO: define 'season' parameter :
-      var season = '16';
-
-      return DataService.getTeams(season);
-
-      /*
-      //TODO: >> 'constants' ?!
-      var URL = 'http://api.vttl.be/0.7/index.php?s=vttl';
-      var WSDL = 'http://api.vttl.be/0.7/?wsdl';
-      var NAMESPACEURL = 'http://api.frenoy.net/TabTAPI';
-
-      $.soap({
-        url: URL,
-        type: 'POST',
-        method : 'GetClubs',
-        namespaceQualifier: '',
-        namespaceURL : NAMESPACEURL,   
-        noPrefix : true,
-        elementName : 'GetClubs',
-        appendMethodToURL: false,
-        soap12: false,
-        context: document.body,
-        data: {
-          //TODO: quid the season parameter ?!
-          Season : "16",
-        },
-        success: function (soapResponse) {
-            // var jsn = soapResponse.toJSON();
-            var txt = soapResponse.toString();
-            alert(txt);
-        },
-        error: function (soapResponse) {
-            alert(soapResponse);
-        }
-      });
-      */
+                $scope.ttteams = result;
+                $scope.$apply();
+            },
+            error: function(SOAPResponse) {
+                //TODO: implement error handling ..
+            }
+        });
     };
 
-    $scope.fetchDivisions = function () {
+    $scope.fetchDivisions = function(season) {
+        'use strict';
 
-      //TODO: define 'season' parameter :
-      var season = '16';
+        $.soap({
+            url: VTTLAPI.URL,
+            type: 'POST',
+            method: 'GetDivisions',
+            namespaceQualifier: '',
+            namespaceURL: VTTLAPI.NAMESPACEURL,
+            noPrefix: true,
+            elementName: 'GetDivisions',
+            appendMethodToURL: false,
+            soap12: false,
+            context: document.body,
+            data: {
+                Season: season,
+            },
+            success: function(SOAPResponse) {
+                var result = [];
+                var json = SOAPResponse.toJSON();
+                if (json) {
+                    var count = parseInt(json['#document']['SOAP-ENV:Envelope']['SOAP-ENV:Body']['ns1:GetDivisionsResponse']['ns1:DivisionCount']);
+                    var entries = json['#document']['SOAP-ENV:Envelope']['SOAP-ENV:Body']['ns1:GetDivisionsResponse']['ns1:DivisionEntries'];
 
-      return DataService.getDivisions(season);
+                    result = entries.map(function(entry) {
+                        var di = entry['ns1:DivisionId'];
+                        var dn = entry['ns1:DivisionName'];
+                        return { 'value': di, 'label': di + ' - ' + dn };
+                    });
+                };
 
-      /*
-      //TODO: >> 'constants' ?!
-      var URL = 'http://api.vttl.be/0.7/index.php?s=vttl';
-      var WSDL = 'http://api.vttl.be/0.7/?wsdl';
-      var NAMESPACEURL = 'http://api.frenoy.net/TabTAPI';
-
-      $.soap({
-        url: URL,
-        type: 'POST',
-        method : 'GetDivisions',
-        namespaceQualifier: '',
-        namespaceURL : NAMESPACEURL,   
-        noPrefix : true,
-        elementName : 'GetDivisions',
-        appendMethodToURL: false,
-        soap12: false,
-        context: document.body,
-        data: {
-          //TODO: quid the season parameter ?!          
-          Season : "16",
-        },
-        success: function (soapResponse) {
-            // var jsn = soapResponse.toJSON();
-        },
-        error: function (soapResponse) {
-        }
-      });
-
-      //TODO: replace this result with returned values of SOAP service
-      return ttdivisions;
-      */
+                $scope.ttdivisions = result;
+                $scope.$apply();
+            },
+            error: function(SOAPResponse) {
+                //TODO: implement error handling ..
+            }
+        });
     };
 
-    $scope.fetchWeeks = function () {
-      return ttweeks;
+    $scope.fetchWeeks = function() {
+        var ttweeks = [
+            { value: "1", label: "Week 1" },
+            { value: "2", label: "Week 2" },
+            { value: "3", label: "Week 3" },
+            { value: "4", label: "Week 4" },
+            { value: "5", label: "Week 5" },
+            { value: "6", label: "Week 6" },
+            { value: "7", label: "Week 7" },
+            { value: "8", label: "Week 8" },
+            { value: "9", label: "Week 9" },
+            { value: "10", label: "Week 10" },
+            { value: "11", label: "Week 11" },
+            { value: "12", label: "Week 12" },
+            { value: "13", label: "Week 13" },
+            { value: "14", label: "Week 14" },
+            { value: "15", label: "Week 15" },
+            { value: "16", label: "Week 16" }
+        ];
+
+        $scope.ttweeks = ttweeks;
     };
 
-    $scope.ttteams = $scope.fetchTeams();
-    $scope.ttdivisions = $scope.fetchDivisions();
-    $scope.ttweeks = $scope.fetchWeeks();
+    $scope.fetchDivisions(season);
+    $scope.fetchTeams(season);
+    $scope.fetchWeeks();
 
     $scope.go = function() {
-      $location.path('/matches'); 
+        $location.path('/matches');
     };
-});   
+});
